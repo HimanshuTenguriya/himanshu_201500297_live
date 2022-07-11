f11d95f012776e5741a7e2d62b29c5d1af019a86
HimanshuTenguriya <80037294+HimanshuTenguriya@users.noreply.github.com>
2022-07-11 20:44:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Himanshu Tenguriya";
+document.getElementById("roll-no").innerHTML="201500297";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString();
